8da9e1aea6f9d050358501a1225452ecb28c1527
XavierLeou <12111811@mail.sustech.edu.cn>
2024-04-01 15:36:48 +0800

    修改了code block实现方式

--- src-ui/components/Code.jsx
+++ src-ui/components/Code.jsx
@@ -1,10 +1,44 @@
-import React from "react";
-import Editor from '@monaco-editor/react';
+import React, {useEffect} from "react";
+
+import Editor, { useMonaco } from '@monaco-editor/react';
 import {Tabs, Tab, Card, CardBody, Textarea} from "@nextui-org/react";
 import {Table, TableHeader, TableBody, TableRow, TableColumn, TableCell} from "@nextui-org/react";
 
 
 export default function Code() {
+    const monaco = useMonaco()
+    useEffect(() => {
+        if (monaco) {
+            monaco.editor.defineTheme('myTheme', {
+                base: 'vs-dark',
+                inherit: true,
+                rules: [
+                    { token: 'comment',
+                        foreground: 'ffa500',
+                        fontStyle: 'italic underline'
+                    },
+                    { token: 'comment.js',
+                        foreground: '008800',
+                        fontStyle: 'bold'
+                    },
+                    { token: 'comment.css',
+                        foreground: '0000ff'
+                    }
+                ],
+                colors: {
+                    'editor.foreground': '#F8F8F2',
+                    'editor.background': '#272822',
+                    'editor.selectionBackground': '#49483E',
+                    'editor.lineHighlightBackground': '#3E3D32',
+                    'editorCursor.foreground': '#F8F8F0',
+                    'editorWhitespace.foreground': '#3B3A32',
+                    'editorIndentGuide.background': '#3B3A32',
+                    'editorLineNumber.foreground': '#75715E',
+                    'editorLineNumber.activeForeground': '#F8F8F0',
+                    'editorCursor.background': '#A7A7A7'
+                }
+            });
+    }, [monaco]);
 
     return (
         <div className="flex flex-col h-full">
