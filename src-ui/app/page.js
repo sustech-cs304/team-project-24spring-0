e6efdad5345593f0d3520562cc6f3bf1e63f99d9
Xavier Yuhan Liu <xzzxy2003@foxmail.com>
2024-04-06 16:08:51 +0800

    修正各种card的适应性，然后添加了文件选项，底部card的消息提示

--- src-ui/app/page.js
+++ src-ui/app/page.js
@@ -3,6 +3,7 @@
 import Taskbar from "@/components/Taskbar";
 import Code from "@/components/Code";
 import Register from "@/components/Register";
+import MessageIO from "@/components/MessageIO";
 import {Card, CardBody, Textarea} from "@nextui-org/react";
 
 export default function Home() {
@@ -11,9 +12,9 @@ export default function Home() {
           <Taskbar/>
           <div className='grid grid-cols-7 gap-4 p-2 max-h-[calc(100vh-60px)] w-full'>
 
-              <div className='col-span-5 max-h-[calc(100vh-60px)]'>
+              <div className='col-span-5 '>
 
-                  <div className='grid grid-rows-8 gap-4 h-full grow'>
+                  <div className='grid grid-rows-8 gap-4 max-h-[calc(100vh-60px)] h-screen grow'>
 
                       <div className='row-span-5'>
                           <Card className='h-full w-full'>
@@ -26,7 +27,7 @@ export default function Home() {
                       <div className='row-span-3'>
                           <Card className='h-full w-full'>
                               <CardBody className='h-full w-full'>
-                                  <Code/>
+                                  <MessageIO/>
                               </CardBody>
                           </Card>
                       </div>
