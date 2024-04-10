3c49735637325c855a1901f6163ba926da7629d9
GenshinImpactStarts <12211831@mail.susteh.edu.cn>
2024-04-10 20:07:55 +0800

    fix(ptr): add Send, Sync trait

--- src-tauri/src/utility/ptr.rs
+++ src-tauri/src/utility/ptr.rs
@@ -42,6 +42,9 @@ impl<T> Hash for Ptr<T> {
     }
 }
 
+unsafe impl<T> Send for Ptr<T> {}
+unsafe impl<T> Sync for Ptr<T> {}
+
 impl<T> Ptr<T> {
     pub fn new(t: &T) -> Self {
         Ptr(unsafe { t as *const T as *mut T })
