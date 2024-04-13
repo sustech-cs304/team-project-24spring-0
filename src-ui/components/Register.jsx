1496d32b4950a7466e02c8502642790956f35528
XavierLeou <12111811@mail.sustech.edu.cn>
2024-04-13 20:09:40 +0800

    change register name

--- src-ui/components/Register.jsx
+++ src-ui/components/Register.jsx
@@ -4,132 +4,29 @@ import {Table, TableHeader, TableBody, TableRow, TableColumn, TableCell} from "@
 
 export default function Register() {
     return (
-        <div className="flex flex-col h-full">
-            <Tabs aria-label="Options">
-                <Tab size="small" key="registers" title="Registers" className='grow h-full'>
-                    <Card className='h-full'>
-                        <CardBody>
-                            <Table className='h-full overflow-y-auto'  aria-label="Example static collection table">
-                                <TableHeader>
-                                    <TableColumn>NAME</TableColumn>
-                                    <TableColumn>ROLE</TableColumn>
-                                    <TableColumn>STATUS</TableColumn>
-                                </TableHeader>
-                                <TableBody>
-                                    <TableRow key="1">
-                                        <TableCell>Tony Reichert</TableCell>
-                                        <TableCell>CEO</TableCell>
-                                        <TableCell>Active</TableCell>
-                                    </TableRow>
-                                    <TableRow key="2">
-                                        <TableCell>Zoey Lang</TableCell>
-                                        <TableCell>Technical Lead</TableCell>
-                                        <TableCell>Paused</TableCell>
-                                    </TableRow>
-                                    <TableRow key="3">
-                                        <TableCell>Jane Fisher</TableCell>
-                                        <TableCell>Senior Developer</TableCell>
-                                        <TableCell>Active</TableCell>
-                                    </TableRow>
-                                    <TableRow key="4">
-                                        <TableCell>William Howard</TableCell>
-                                        <TableCell>Community Manager</TableCell>
-                                        <TableCell>Vacation</TableCell>
-                                    </TableRow>
-                                    <TableRow key="5">
-                                        <TableCell>William Howard</TableCell>
-                                        <TableCell>Community Manager</TableCell>
-                                        <TableCell>Vacation</TableCell>
-                                    </TableRow>
-                                </TableBody>
-                            </Table>
-
-                        </CardBody>
-                    </Card>
-                </Tab>
-                <Tab key="coproc1" title="Coproc 1" className='grow h-full'>
-                    <Card className='h-full'>
-                        <CardBody>
-                            <Table className='h-full overflow-y-auto'  aria-label="Example static collection table">
-                                <TableHeader>
-                                    <TableColumn>NAME</TableColumn>
-                                    <TableColumn>ROLE</TableColumn>
-                                    <TableColumn>STATUS</TableColumn>
-                                </TableHeader>
-                                <TableBody>
-                                    <TableRow key="1">
-                                        <TableCell>Tony Reichert</TableCell>
-                                        <TableCell>CEO</TableCell>
-                                        <TableCell>Active</TableCell>
-                                    </TableRow>
-                                    <TableRow key="2">
-                                        <TableCell>Zoey Lang</TableCell>
-                                        <TableCell>Technical Lead</TableCell>
-                                        <TableCell>Paused</TableCell>
-                                    </TableRow>
-                                    <TableRow key="3">
-                                        <TableCell>Jane Fisher</TableCell>
-                                        <TableCell>Senior Developer</TableCell>
-                                        <TableCell>Active</TableCell>
-                                    </TableRow>
-                                    <TableRow key="4">
-                                        <TableCell>William Howard</TableCell>
-                                        <TableCell>Community Manager</TableCell>
-                                        <TableCell>Vacation</TableCell>
-                                    </TableRow>
-                                    <TableRow key="5">
-                                        <TableCell>William Howard</TableCell>
-                                        <TableCell>Community Manager</TableCell>
-                                        <TableCell>Vacation</TableCell>
-                                    </TableRow>
-                                </TableBody>
-                            </Table>
-
-                        </CardBody>
-                    </Card>
-                </Tab>
-                <Tab key="coproc2" title="Coproc 2" className='grow h-full'>
-                    <Card className='h-full'>
-                        <CardBody>
-                            <Table className='h-full overflow-y-auto'  aria-label="Example static collection table">
-                                <TableHeader>
-                                    <TableColumn>NAME</TableColumn>
-                                    <TableColumn>ROLE</TableColumn>
-                                    <TableColumn>STATUS</TableColumn>
-                                </TableHeader>
-                                <TableBody>
-                                    <TableRow key="1">
-                                        <TableCell>Tony Reichert</TableCell>
-                                        <TableCell>CEO</TableCell>
-                                        <TableCell>Active</TableCell>
-                                    </TableRow>
-                                    <TableRow key="2">
-                                        <TableCell>Zoey Lang</TableCell>
-                                        <TableCell>Technical Lead</TableCell>
-                                        <TableCell>Paused</TableCell>
-                                    </TableRow>
-                                    <TableRow key="3">
-                                        <TableCell>Jane Fisher</TableCell>
-                                        <TableCell>Senior Developer</TableCell>
-                                        <TableCell>Active</TableCell>
-                                    </TableRow>
-                                    <TableRow key="4">
-                                        <TableCell>William Howard</TableCell>
-                                        <TableCell>Community Manager</TableCell>
-                                        <TableCell>Vacation</TableCell>
-                                    </TableRow>
-                                    <TableRow key="5">
-                                        <TableCell>William Howard</TableCell>
-                                        <TableCell>Community Manager</TableCell>
-                                        <TableCell>Vacation</TableCell>
-                                    </TableRow>
-                                </TableBody>
-                            </Table>
-
-                        </CardBody>
-                    </Card>
-                </Tab>
-            </Tabs>
+        <div className="flex flex-col max-h-[calc(100vh-60px)] overflow-scroll">
+            <Card className='h-full'>
+                <CardBody className='h-full'>
+                    <Table className='h-full'  aria-label="Example static collection table">
+                        <TableHeader>
+                            <TableColumn>Register</TableColumn>
+                            <TableColumn>Value</TableColumn>
+                        </TableHeader>
+                        <TableBody>
+                            <TableRow key="0">
+                                <TableCell>x0</TableCell>
+                                <TableCell>0</TableCell>
+                            </TableRow>
+                            {Array.from({ length: 31 }, (_, index) => (
+                                <TableRow key={index}>
+                                    <TableCell>{`x${index}`}</TableCell>
+                                    <TableCell>0</TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </CardBody>
+            </Card>
         </div>
     );
 }
