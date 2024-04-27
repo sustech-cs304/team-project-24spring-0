d0f27f2037d84623b952f59d8a97dabca9de2f33
XavierLeou <12111811@mail.sustech.edu.cn>
2024-04-27 19:39:20 +0800

    add run button

--- src-ui/components/MultifileCode.jsx
+++ src-ui/components/MultifileCode.jsx
@@ -15,11 +15,14 @@ export default function MultifileCode() {
     return (
         <Tabs size="small" aria-label="Files">
             {files.map(file => (
-                <Tab key={file.fileName} title={file.fileName + (file.code!=file.original?' *':"")} className="h-full">
+                <Tab key={file.fileName} title={file.fileName.split('/').pop() + (file.code!=file.original?' *':"")} className="h-full">
                     <div className="h-full w-full relative">
                         <Code fileName={file.fileName}/>
                         <div className='absolute right-4 top-2 flex-row gap-2'>
-                            <Button color="danger" size="sm" onClick={() => deleteFile(file.fileName)}>Delete</Button>
+                            <ButtonGroup>
+                                <Button color="success" size="sm">Run</Button>
+                                <Button color="danger" size="sm" onClick={() => deleteFile(file.fileName)}>Delete</Button>
+                            </ButtonGroup>
                         </div>
                     </div>
                 </Tab>
--- src-ui/utils/state.js
+++ src-ui/utils/state.js
@@ -3,7 +3,7 @@ import { create } from 'zustand';
 // file = { fileName, code }
 // fileName should be unique when adding a file
 const useFileStore = create((set) => ({
-    files: [{fileName: 'untitled.S', code: 'Hello, world!', original: "Hello, world!", runLines: ['r1']}],
+    files: [{fileName: '/untitled.S', code: 'Hello, world!', original: "Hello, world!", runLines: ['r1']}],
 
     addFile: (file) => set(state => ({ files: [...state.files, file] })),
     deleteFile: (fileName) => set(state => ({ files: state.files.filter(file => file.fileName !== fileName) })),
