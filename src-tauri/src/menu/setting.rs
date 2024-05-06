5d4cb65aaec45de175d2b6da5981d1f266dc5339
Vollate <uint44t@gmail.com>
2024-05-06 13:10:08 +0800

    [Refactor]: APIs (#76)
    * feat(many handle):
    - finish exit and close menu handle
    - fix file save logic
    
    * fix(rpc server handle): add pointer to rpc handler
    
    * refactor(APIs): refactor multiple api for frontend and backend

--- src-tauri/src/menu/setting.rs
+++ src-tauri/src/menu/setting.rs
@@ -9,8 +9,17 @@ pub fn new() -> Submenu {
 
 pub fn event_handler(event: WindowMenuEvent) {
     match event.menu_item_id().strip_prefix("setting_").unwrap() {
+        "assembler_memory" => {
+            assembler_memory_handler(event);
+        }
         _ => {
             println!("Unknown file menu item {}", event.menu_item_id());
         }
     }
+}
+
+fn assembler_memory_handler(event: WindowMenuEvent) {
+    todo!("update assembler memory setting");
+    // you could reference other files in menu folder about how to use the event
+    // don't forage to write the comment to generate doc for frontend
 }
