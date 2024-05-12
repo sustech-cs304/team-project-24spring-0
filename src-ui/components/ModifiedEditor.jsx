59874653e99239a2427fc2ff986d2306ebf10111
Xavier Yuhan Liu <xzzxy2003@foxmail.com>
2024-05-12 16:57:43 +0800

    implement output of current input and its location

--- src-ui/components/ModifiedEditor.jsx
+++ src-ui/components/ModifiedEditor.jsx
@@ -1,63 +1,56 @@
 import Editor, {useMonaco} from "@monaco-editor/react";
-import React, {useEffect} from "react";
+import React, {useEffect, useRef} from "react";
 import Image from "next/image";
 import { invoke } from '@tauri-apps/api/tauri';
 import useOutputStore from "@/utils/outputState";
 import useFileStore from "@/utils/state";
-import { data } from "autoprefixer";
+
+function getDifference(a, b)
+{
+    var i = 0;
+    var j = 0;
+    var result = "";
+
+    while (j < b.length)
+    {
+     if (a[i] != b[j] || i == a.length)
+         result += b[j];
+     else
+         i++;
+     j++;
+    }
+    return result;
+}
 
 
 export default function ModifiedEditor({fileName}) {
-    const monaco = useMonaco();
+    const monacoRef = useRef(null);
+    const editorRef = useRef(null);
     const state = useFileStore();
     const file = useFileStore(state => state.files.find(file => file.fileName === fileName));
-    useEffect(() => {
-        if (monaco) {
-            monaco.editor.defineTheme('myTheme', {
-                base: 'vs-dark',
-                inherit: true,
-                rules: [
-                    {
-                        token: 'comment',
-                        foreground: 'ffa500',
-                        fontStyle: 'italic underline'
-                    },
-                    {
-                        token: 'comment.js',
-                        foreground: '008800',
-                        fontStyle: 'bold'
-                    },
-                    {
-                        token: 'comment.css',
-                        foreground: '0000ff'
-                    }
-                ],
-                colors: {
-                    'editor.foreground': '#F8F8F2',
-                    'editor.background': '#272822',
-                    'editor.selectionBackground': '#49483E',
-                    'editor.lineHighlightBackground': '#3E3D32',
-                    'editorCursor.foreground': '#F8F8F0',
-                    'editorWhitespace.foreground': '#3B3A32',
-                    'editorIndentGuide.background': '#3B3A32',
-                    'editorLineNumber.foreground': '#75715E',
-                    'editorLineNumber.activeForeground': '#F8F8F0',
-                    'editorCursor.background': '#A7A7A7'
-                }
-            });
-        }
-    }, [monaco]);
+
+    function handleEditorDidMount(editor, monaco) {
+        // here is the editor instance
+        // you can store it in `useRef` for further usage
+        editorRef.current = editor;
+        monacoRef.current = monaco;
+    }
+    
 
     var handleEditorChange = async (value) => {
+        let newInput = getDifference(file.code, value);
         state.updateFile(fileName, value, file.original, file.assembly, file.runLines);
         const result = await invoke('update_tab', {filepath: fileName, data: value});
-        console.log('update tab result: ', result);
+        // console.log('update tab result: ', result);
         if(!result.success){
             console.log('Error updating tab');
             const outputStore = useOutputStore.getState();
             outputStore.addOutput('Error updating tab: ' + fileName);
         }
-
+        let position = editorRef.current.getPosition();
+        let line = position.lineNumber;
+        let column = position.column;
+        console.log('Current: line: ', line, 'column: ', column, 'value: ', newInput);
     }
 
     return (
@@ -67,6 +60,7 @@ export default function ModifiedEditor({fileName}) {
             className='overflow-hidden h-full'
             value={file.code}
             onChange={handleEditorChange}
+            onMount={handleEditorDidMount}
             />
             <div className='absolute right-2 top-0 flex-row gap-2'>
                 <button className='bg-gray-100 rounded-2xl hover:bg-gray-200'>
