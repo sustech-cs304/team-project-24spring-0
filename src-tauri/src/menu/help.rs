92d2e8242fe09d41472e96da4e9f59b520cc91b1
Xavier Yuhan Liu <12111811@mail.sustech.edu.cn>
2024-05-18 16:31:42 +0800

    cargo fmt

--- src-tauri/src/menu/help.rs
+++ src-tauri/src/menu/help.rs
@@ -21,9 +21,7 @@ fn manual_handler(_event: &WindowMenuEvent) {
             app_handle,
             "manual", /* the unique window label */
             tauri::WindowUrl::External(
-                "http://localhost:3000/riscv-spec-v2.2.pdf"
-                    .parse()
-                    .unwrap(),
+                "http://localhost:3000/riscv-spec-v2.2.pdf".parse().unwrap(),
             ),
         )
         .title("Manual")
