d592b45a59d00928a7fca89d234ea82e05898ab7
Xavier Yuhan Liu <xzzxy2003@foxmail.com>
2024-05-24 23:59:35 +0800

    完成增加和删除断点的功能

--- src-ui/components/CodeLineTable.jsx
+++ src-ui/components/CodeLineTable.jsx
@@ -7,12 +7,41 @@ import useOutputStore from "@/utils/outputState";
 export default function CodeLineTable({fileName}) {
     const store = useFileStore();
     const file = store.files.find(file => file.fileName === fileName);
-    var lines = file.assembly;
+    let lines = file.assembly;
+    const outputStore = useOutputStore.getState();
 
-    var handleSelectionChange = (selectedKeys) => {
+    var handleSelectionChange = async (selectedKeys) => {
         store.setSelectedLines(fileName, selectedKeys);
-        console.log('selected keys', selectedKeys);
+        if (selectedKeys === 'all') {
+            console.log('all selected');
+            for (let line in lines) {
+                if (!(line in file.selectedLines)){
+                    const result = await invoke('set_breakpoint', {line: parseInt(line)});
+                    outputStore.addOutput('Breakpoint set at line ' + line);
+                }
+            }
+        } else {
+            // selectedKeys is a set
+            // get all the content in the set
+            let selectedLines = Array.from(selectedKeys);
+            console.log('selected keys', selectedKeys);
+            if (selectedKeys.anchorKey === undefined){
+                console.log('remove all');
+                for (let line in lines) {
+                    const result = await invoke('remove_breakpoint', {line: parseInt(line)});
+                    outputStore.addOutput('Breakpoint removed at line ' + line);
+                }
+            } else if (selectedLines.find(key => key === selectedKeys.anchorKey.toString())){
+                const result = await invoke('set_breakpoint', {line: parseInt(selectedKeys.anchorKey)});
+                outputStore.addOutput('Breakpoint set at line ' + selectedKeys.anchorKey);
+            } else {
+                const result = await invoke('remove_breakpoint', {line: parseInt(selectedKeys.anchorKey)});
+                outputStore.addOutput('Breakpoint removed at line ' + selectedKeys.anchorKey);
+            }
+        }
+
     }
+
 
     var handleStep = async () => {
         console.log("Step Executed")
