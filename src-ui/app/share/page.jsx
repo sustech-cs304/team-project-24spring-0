9cf031e14dd70218356586ba97eac72392ff0e37
Xavier Yuhan Liu <xzzxy2003@foxmail.com>
2024-05-27 22:44:07 +0800

    authorize接口前端实现

--- src-ui/app/share/page.jsx
+++ src-ui/app/share/page.jsx
@@ -23,6 +23,9 @@ export default function Share() {
   var [inSession, setInSession] = useState(false)
   var [port, setPort] = useState('')
   var [password, setPassword] = useState('')
+  var [ip, setIp] = useState('')
+  var [port2, setPort2] = useState('')
+  var [password2, setPassword2] = useState('')
 
   var sessionID = '123456'
   function handleSession() {
@@ -42,6 +45,23 @@ export default function Share() {
     setPassword(value)
   }
 
+  const handleIpChange = value => {
+    setIp(value)
+  }
+
+  const handlePort2Change = value => {
+    var inputPort = parseInt(value)
+    if ((inputPort <= 65535 && inputPort >= 0) || isNaN(inputPort)) {
+      setPort2(inputPort)
+    } else {
+      alert('Port should be within 0 and 65535')
+    }
+  }
+
+  const handlePassword2Change = value => {
+    setPassword2(value)
+  }
+
   const onServerButtonClick = async () => {
     if (inSession) {
       // end server
@@ -56,6 +76,7 @@ export default function Share() {
     } else {
       // check whether port and pwd are neither null
       if (isNaN(port) || password == '') {
+        alert('Port and password should not be empty!')
         return
       }
       // start server
@@ -70,6 +91,35 @@ export default function Share() {
       } else {
         alert('Failed to start share sever. Reason: ' + result.message + '.')
       }
+    }
+  }
+
+  const onAuthorize = async () => {
+    if (ip == '' || isNaN(port2) || password2 == '') {
+      alert('IP, port and password should not be empty!')
+      return
+    }
+
+    // check whether ip is a valid ipv4 address
+    var ipPattern = new RegExp(
+      '^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$',
+    )
+    if (!ipPattern.test(ip)) {
+      alert('IP is not a valid ipv4 address!')
+      return
+    }
+
+    var result = await invoke('authorize', {
+      ip: ip,
+      port: port2,
+      password: password2,
+    })
+    console.log(result)
+    if (result.success) {
+      setInSession(!inSession)
+      alert('Authorize successfully!')
+    } else {
+      alert('Failed to authorize. Reason: ' + result.message + '.')
     }
   }
 
@@ -115,10 +165,37 @@ export default function Share() {
             </Card>
           </Tab>
           <Tab key="Join" title="Join">
-            <Textarea placeholder="Enter Session ID" className="py-4" />
-            <Button className="p-2 w-full" color="primary" onClick={() => handleSession()}>
-              {inSession ? 'Join Session' : 'Leave Session'}
-            </Button>
+            <Card>
+              <CardBody>
+                <div className="w-full flex flex-col gap-4">
+                  <div className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4">
+                    <Input type="text" label="IP" onValueChange={handleIpChange} value={ip} />
+                  </div>
+                  <div className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4">
+                    <Input
+                      type="number"
+                      label="Port"
+                      onValueChange={handlePort2Change}
+                      value={port2}
+                    />
+                  </div>
+                  <div className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4">
+                    <Input
+                      type="password"
+                      label="Password"
+                      onValueChange={handlePassword2Change}
+                      value={password2}
+                    />
+                  </div>
+                </div>
+              </CardBody>
+              <Divider />
+              <CardFooter>
+                <Button className="p-2 w-full" color="primary" onClick={() => onAuthorize()}>
+                  Authorize
+                </Button>
+              </CardFooter>
+            </Card>
           </Tab>
         </Tabs>
       </CardBody>
