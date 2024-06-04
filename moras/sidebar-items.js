7500a17fb02e3479c60e8ba93c7468ea7335fa39
vollate <vollate@users.noreply.github.com>
2024-06-04 02:04:13 +0000

    deploy: eb4c0d8c3cd590d5c94e0c43d5213a172ba85a36

--- moras/sidebar-items.js
+++ moras/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["main"],"macro":["binstimpl","create_menu","dprintln","iinstimpl","r4instimpl","rinstimpl","sinstimpl","uinstimpl"],"mod":["interface","io","menu","modules","remote","simulator","storage","types","utility"],"static":["APP_HANDLE","CURSOR_LIST"]};
+window.SIDEBAR_ITEMS = {"fn":["main"],"macro":["binstimpl","create_menu","dprintln","iinstimpl","r4instimpl","rinstimpl","sinstimpl","uinstimpl"],"mod":["interface","io","menu","modules","remote","simulator","storage","types","utility"],"static":["APP_HANDLE","CURSOR_LIST","HISTORY"]};
